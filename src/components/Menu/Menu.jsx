b3c7324d2fe8d034731ec2bbc413ec93e8cf015b
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-05-02 23:08:22 +0500

    Create Menu.jsx

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+import './Menu.scss';
+
+const Menu = ({ menuOpen, setMenuOpen }) => {
+  return (
+    <div className={`menu ${menuOpen ? 'active' : ''}`}>
+      <ul>
+        <li onClick={() => setMenuOpen(false)}>
+          <Link to="/intro">Home</Link>
+        </li>
+        <li onClick={() => setMenuOpen(false)}>
+          <Link to="/portfolio">Portfolio</Link>
+        </li>
+        <li onClick={() => setMenuOpen(false)}>
+          <Link to="/works">Works</Link>
+        </li>
+        <li onClick={() => setMenuOpen(false)}>
+          <Link to="/testimonials">Testimonials</Link>
+        </li>
+        <li onClick={() => setMenuOpen(false)}>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Menu;
