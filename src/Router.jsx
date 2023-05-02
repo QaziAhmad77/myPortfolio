bb62d0f6b67b408ecd15553372e4be4f42710ecb
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-05-02 23:09:02 +0500

    Update Router.jsx

--- src/Router.jsx
+++ src/Router.jsx
@@ -4,19 +4,25 @@ import Portfolio from './components/Portfolio/Portfolio';
 import Works from './components/Works/Works';
 import Testimonials from './components/Testimonials/Testimonials';
 import Contact from './components/Contact/Contact';
+import Menu from './components/Menu/Menu';
 import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 const Router = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <>
-      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
-        <Intro />
-        <Portfolio />
-        <Works />
-        <Testimonials />
-        <Contact />
+        <Routes>
+          <Route path="/intro" element={<Intro />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/works" element={<Works />} />
+          <Route path="/testimonials" element={<Testimonials />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </div>
     </>
   );
