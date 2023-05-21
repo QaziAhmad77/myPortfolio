d03df3ede7b26d117ed8646a46a1e251f3a57892
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-05-21 23:40:23 +0500

    Update Router.jsx

--- src/Router.jsx
+++ src/Router.jsx
@@ -20,14 +20,42 @@ const Router = () => {
         setColor={setColor}
       />
       <div className="sections">
-        <Intro id="intro-section" color={color} setColor={setColor} />
-        <Portfolio id="portfolio-section" />
-        <Works id="work-section" />
-        <Testimonials id="testimonials-section" />
-        <Contact id="contact-section" />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Intro color={color} setColor={setColor} /> <Portfolio />{' '}
+                <Works /> <Testimonials /> <Contact />
+              </>
+            }
+          />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/works" element={<Works />} />
+          <Route path="/testimonials" element={<Testimonials />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </div>
     </>
   );
 };
 
 export default Router;
+
+{
+  /* <>
+<Topbar
+  menuOpen={menuOpen}
+  setMenuOpen={setMenuOpen}
+  color={color}
+  setColor={setColor}
+/>
+<div className="sections">
+  <Intro id="intro-section" color={color} setColor={setColor} />
+  <Portfolio id="portfolio-section" />
+  <Works id="work-section" />
+  <Testimonials id="testimonials-section" />
+  <Contact id="contact-section" />
+</div>
+</> */
+}
